9b6b7772e7e9008a6bd6c939304d6b4a0013d765
Skale331 <stepan.s.matios2@gmail.com>
2023-07-05 12:46:47 +0300

    changed model properites

--- Assets/Scripts/first_person_movement.cs
+++ Assets/Scripts/first_person_movement.cs
@@ -7,14 +7,26 @@ public class first_person_movement : MonoBehaviour
     [SerializeField] public float mvSpeed = 5f;
     private Vector2 velocity;
 
+
+    [SerializeField] public float jumpStrength = 2f;
+    [SerializeField] private float maxGroundDistance = 0.3f;
+
+    private Rigidbody _rigidbody;
+    private Transform _groundCheckObject;
+
+    private bool isground;
+
+
     PhotonView view;
     public GameObject Camera;
     public first_person_movement scriptPlayerController;
    
     private void Awake()
     {
-        view = GetComponent<PhotonView>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _groundCheckObject = GameObject.FindGameObjectWithTag("GroundCheck").transform;
 
+        view = GetComponent<PhotonView>();
         if(!view.IsMine) 
         {
             Camera.SetActive(false);
@@ -28,5 +40,11 @@ public class first_person_movement : MonoBehaviour
         velocity.y = Input.GetAxis("Vertical") * mvSpeed * Time.deltaTime; 
 
         transform.Translate(velocity.x, 0f,velocity.y);
+
+        isground = Physics.Raycast(_groundCheckObject.transform.position, Vector3.down, maxGroundDistance);
+
+        if (Input.GetButtonDown("Jump") && isground)
+            _rigidbody.AddForce(Vector3.up * 100 * jumpStrength);
+
     }
 }
