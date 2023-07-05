adb216adb559742f772c8d9ad6b53e8905172158
Skale331 <stepan.s.matios2@gmail.com>
2023-07-05 14:56:42 +0300

    test_choosing_character

---
+++ Assets/Scripts/character.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character : MonoBehaviour
+{
+
+    private int i;
+    public GameObject[] AllCharacters;
+
+    private void Start()
+    {
+        i = PlayerPrefs.GetInt("CurrentCharacter");
+        AllCharacters[i].SetActive(true);
+
+    }
+
+}
---
+++ Assets/Scripts/select_character.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class select_character : MonoBehaviour
+{
+
+    private int i;
+    private int currentCharacter;
+
+    public GameObject[] AllCharacters;
+    public GameObject ArrowRight;
+    public GameObject ArrowLeft;
+    public GameObject ButtonSelectCharatcer;
+    public GameObject TextSelectCharatcer;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("CurrentlyCharacter"))
+        {
+            i = PlayerPrefs.GetInt("CurrentlyCharacter");
+            currentCharacter = PlayerPrefs.GetInt("CurrentlyCharacter");
+
+            ButtonSelectCharatcer.SetActive(false);
+            TextSelectCharatcer.SetActive(true);
+        }
+        else PlayerPrefs.SetInt("CurrentlyCharacter", i);
+
+        AllCharacters[i].SetActive(true);
+        
+        if(i > 0)
+        {
+            ArrowLeft.SetActive(true);
+        }
+        if(i == AllCharacters.Length) 
+        {
+            ArrowRight.SetActive(false);
+        }
+    }
+
+    public void Arrow_Right()
+    {
+        if(i<AllCharacters.Length)
+        {
+            if (i == 0)
+            {
+                ArrowLeft.SetActive(true);
+            }
+            AllCharacters[i].SetActive(false);
+            i++;
+            AllCharacters[i].SetActive(true);
+
+            if(currentCharacter == i)
+            {
+                ButtonSelectCharatcer.SetActive(false);
+                TextSelectCharatcer.SetActive(true);
+            }
+            else
+            {
+                ButtonSelectCharatcer.SetActive(true);
+                TextSelectCharatcer.SetActive(false);
+            }
+            if(i+1 == AllCharacters.Length)
+            {
+                ArrowRight.SetActive(false);
+            }
+        }
+    }
+
+    public void Arrow_Left()
+    {
+        if (i < AllCharacters.Length)
+        {
+            AllCharacters[i].SetActive(false);
+            i--;
+            AllCharacters[i].SetActive(true);
+            ArrowRight.SetActive(true);
+
+
+            if (currentCharacter == i)
+            {
+                ButtonSelectCharatcer.SetActive(false);
+                TextSelectCharatcer.SetActive(true);
+            }
+            else
+            {
+                ButtonSelectCharatcer.SetActive(true);
+                TextSelectCharatcer.SetActive(false);
+            }
+
+            if (i == 0)
+            {
+                ArrowLeft.SetActive(false);
+            }
+        }
+    }
+
+    public void SelectCharacter()
+    {
+        PlayerPrefs.SetInt("CurrentCharacter", i);
+        currentCharacter = i;
+        ButtonSelectCharatcer.SetActive(false);
+        TextSelectCharatcer.SetActive(true);
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+}
