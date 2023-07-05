a8b67bc826c500d3b2dd66c54a8027a9ba40c27f
Skale331 <stepan.s.matios2@gmail.com>
2023-07-05 08:21:05 +0300

    loading+menu+connecting

---
+++ Assets/Scripts/main_menu.cs
@@ -0,0 +1,41 @@
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviourPunCallbacks
+{
+    public InputField InputFieldNAMEROOM;
+    public GameObject ErrorPanel;
+    private bool firstStart = true;
+    
+    public void CreateRoom()
+    {
+    ErrorPanel.SetActive(false);
+        if(InputFieldNAMEROOM.text.Length > 3 || firstStart == true)
+        {
+            RoomOptions roomOptions = new RoomOptions();
+            roomOptions.MaxPlayers = 4;
+            PhotonNetwork.CreateRoom(InputFieldNAMEROOM.text, roomOptions);
+            firstStart= false;
+        }
+        else ErrorPanel.SetActive(true);
+
+    }
+
+    public void JoinRoom()
+    {
+        if (InputFieldNAMEROOM.text.Length > 3 || firstStart == true) 
+        {
+            PhotonNetwork.JoinRoom(InputFieldNAMEROOM.text);
+            firstStart = false;
+        } 
+        else ErrorPanel.SetActive(true);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game");
+    }
+
+}
