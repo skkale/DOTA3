d00b536470b0338ddd407b6315fa810a261ce312
Skale331 <stepan.s.matios2@gmail.com>
2023-07-10 10:28:43 +0300

    bug_fix_1

--- Assets/Scripts/hp_bar.cs
+++ Assets/Scripts/hp_bar.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
 
 public class hp_bar : MonoBehaviour
 {
+    PhotonView view;
     public float hp = 100f;
     [SerializeField]
     public Image bar;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Death")
+        if (other.gameObject.tag == "Death" && view.IsMine)
         {
             hp -= 5;
             bar.fillAmount = hp / 100;
