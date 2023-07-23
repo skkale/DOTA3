2327dbfc76932638996c65397b46e08428bdbbd3
Skale331 <stepan.s.matios2@gmail.com>
2023-07-23 20:48:36 +0300

    sync_animation

--- Assets/Scripts/PlayerAnimator1.cs
+++ Assets/Scripts/PlayerAnimator1.cs
@@ -1,16 +1,22 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAnimator1 : MonoBehaviour
 {
+    PhotonView view;
     Animator anim;
     private void Start()
     {
+        view = GetComponent<PhotonView>();
         anim = GetComponent<Animator>();
     }
     void Update()
     {
+        if (!view.IsMine)
+            return;
+
         if (Input.GetKey(KeyCode.W))
             anim.SetTrigger("Run");
 
